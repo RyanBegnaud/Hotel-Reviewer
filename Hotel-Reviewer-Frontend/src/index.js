d2839d62b54175bbd80871c9f36e038a0af03219
RyanBegnaud <Ryanbegnaud13@gmail.com>
2021-01-20 17:29:07 -0800

    added changes to css styling on imgs and h2's

---
+++ Hotel-Reviewer-Frontend/src/index.js
@@ -0,0 +1,20 @@
+const hotelUrl = "http://localhost:3000/hotels"
+const proxyurl = "https://cors-anywhere.herokuapp.com/";
+
+document.addEventListener('DOMContentLoaded', 
+getHotels(),
+)
+
+function getHotels(hotels) {
+    fetch("http://localhost:3000/hotels")
+    .then(resp => resp.json())
+    .then(hotels =>  hotels.forEach(hotel => makeHotel(hotel)))
+}
+
+function makeHotel(hotel) {
+    let body = document.querySelector("body")
+    let newDiv = document.createElement("div")
+    let imgArr = hotel.imgs.split(",")
+    newDiv.innerHTML = `<h2>${hotel.name}</h2><p>Located on: ${hotel.island}</p><p>Address: ${hotel.address}</p><img src=${imgArr[0]}><img src=${imgArr[1]}><br><form method="GET" ><input type="submit" value="show" id="button"></form>`
+    body.append(newDiv)
+}
