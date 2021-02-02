292533a7156be712be802b6bbd3ada63d3710d86
RyanBegnaud <Ryanbegnaud13@gmail.com>
2021-02-02 14:16:47 -0800

    added modal for intended sign ins

--- Hotel-Reviewer-Frontend/src/index.js
+++ Hotel-Reviewer-Frontend/src/index.js
@@ -1,9 +1,10 @@
 const hotelUrl = "http://localhost:3000/hotels"
 const proxyurl = "https://cors-anywhere.herokuapp.com/";
-
-document.addEventListener('DOMContentLoaded', 
-getHotels(),
-)
+document.addEventListener('DOMContentLoaded', getHotels())
+document.addEventListener("DOMContentLoaded", () =>{
+    const signIn = document.querySelector("button.btnIn")
+    signIn.addEventListener("click", () => addListeners())
+})
 
 function getHotels(hotels) {
     fetch("http://localhost:3000/hotels")
@@ -12,9 +13,33 @@ function getHotels(hotels) {
 }
 
 function makeHotel(hotel) {
-    let body = document.querySelector("body")
-    let newDiv = document.createElement("div")
-    let imgArr = hotel.imgs.split(",")
+    const body = document.querySelector("body")
+    const newDiv = document.createElement("div")
+    const imgArr = hotel.imgs.split(",")
     newDiv.innerHTML = `<h2>${hotel.name}</h2><p>Located on: ${hotel.island}</p><p>Address: ${hotel.address}</p><img src=${imgArr[0]}><img src=${imgArr[1]}><br><button type="button"></button>`
     body.append(newDiv)
+}
+
+const addListeners = () => {
+    const signIn = document.querySelector("button.btnIn")
+    const signUp = document.querySelector("button.btnUp")
+    const form = document.createElement("form")
+    const input = document.createElement("input")
+
+    form.setAttribute("name", "sign-in")
+    input.innerHTML = "UserName:"
+    form.appendChild(input)
+    signIn.addEventListener("click", alert(`<form name="sign-in"><p>Enter UserName!: <input type="text">`))
+}
+
+// const signUps = () => {
+//     const signIn = document.querySelector("button.sign-in")
+//     const signUp = document.querySelector("button.sign-up")
+//     signUp.innerHTML = "lol"
+//     signIn.addEventListener("click", showForm)
+//     signUp.addEventListener("click", showForm)   
+// }
+
+const showForm = () => {
+    
 }
