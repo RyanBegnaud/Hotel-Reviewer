11e4398df5b43fd5da085bf763d3861c02bf6666
RyanBegnaud <Ryanbegnaud13@gmail.com>
2021-02-10 11:46:39 -0800

    added fetch function to create a review

--- Hotel-Reviewer-Api/app/controllers/reviews_controller.rb
+++ Hotel-Reviewer-Api/app/controllers/reviews_controller.rb
@@ -1,2 +1,24 @@
 class ReviewsController < ApplicationController
+    def index
+        reviews = Review.all 
+        render json: reviews 
+    end
+
+    def create
+        if params[:user_id] == nil 
+            flash[:notice] = "Must be signed in to leave a review"
+        else 
+            review = Review.create(review_params)
+            hotel = Hotel.find_by(id: params[:hotel_id])
+            hotel.total_ratings 
+            render json: review 
+        end
+    end
+
+    private 
+
+    def review_params
+        params.require(:review).permit(:hotel_id, :user_id, :rating)
+    end
+        
 end
